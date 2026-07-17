a49d54016fd89a7c2e5c924f6f2dee6a3d2a4d7c
Arlo <omidiarad81@gmail.com>
2026-07-17 18:07:18 +0330

    Add files via upload

---
+++ create_model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score , classification_report , confusion_matrix
+import joblib
+
+Coefficient = 98.7
+model_name = "my_model.pkl"
+
+
+data = pd.read_csv('Ai_training/Loan_Approval_Predictor/loan_dataset.csv')
+data = data.dropna()
+
+x = data.drop(columns=['Approved'])
+y=data['Approved']
+
+x_train , x_test , y_train , y_test =train_test_split(x , y , test_size=0.2 )
+
+while True:
+    model=RandomForestClassifier(n_estimators=100 )
+    model.fit(x_train , y_train)
+    
+    y_pr = model.predict(x_test)
+    acc = accuracy_score(y_test , y_pr)
+    print(acc)
+    if acc*100 >Coefficient:
+        print("----------------finish-------------------")
+        joblib.dump(model , model_name)
+        break
+    # print(classification_report(y_test , y_pr))
+    
---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import joblib
+
+modle_addres  = "Ai_training/Loan_Approval_Predictor/my_model.pkl"
+model = joblib.load(modle_addres)
+
+
+income = int(input("Monthly income : "))         
+credit_score = int(input("credit_score : "))   
+loan_amount = int(input("loan_amount : "))     
+work_exp = int(input("work_exp : "))        
+has_default = int(input("has_default : "))      
+
+dti_ratio = (loan_amount * 0.035) / income
+
+applicant_data = pd.DataFrame([{
+    'Monthly_Income_M': income,
+    'Credit_Score': credit_score,
+    'Loan_Amount_M': loan_amount,
+    'Work_Experience_Years': work_exp,
+    'Has_Prior_Default': has_default,
+    'DTI_Ratio': dti_ratio
+}])
+
+prediction = model.predict(applicant_data)[0]
+probability = model.predict_proba(applicant_data)[0][1] * 100
+
+print("-----------------Answer-------------------")
+if prediction == 1:
+    print(f"The loan has been approved. {probability:.1f}%)")
+else:
+    print(f"The loan application was rejected. {probability:.1f}%)")
